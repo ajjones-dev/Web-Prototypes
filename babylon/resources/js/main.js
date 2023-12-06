9415454fd642c56113e44cc4655963c81219f0bc
ajjones.dev <ajjones.dev@gmail.com>
2023-12-06 15:54:12 -0600

    Start creating a scene

--- babylon/resources/js/main.js
+++ babylon/resources/js/main.js
@@ -3,32 +3,61 @@
 const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true);
 
+function createCamera(scene) {
+    const camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 15, BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas);
+
+    camera.lowerRadiusLimit = 6;
+    camera.upperRadiusLimit = 20;
+}
+
+function createLight(scene) {
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
+    light.intensity = 0.5;
+    light.groundColor = new BABYLON.Color3(0,0,1);
+}
+
+function createSun(scene) {
+    const sunMaterial = new BABYLON.StandardMaterial('sunMaterial', scene)
+    sunMaterial.emissiveTexture = new BABYLON.Texture('assets/images/sun.jpg', scene)
+    sunMaterial.diffuseColor = BABYLON.Color3.Black();
+    sunMaterial.specularColor = BABYLON.Color3.Black();
+
+    const sun = BABYLON.MeshBuilder.CreateSphere('sun', {
+        segments: 16,
+        diameter: 4
+    }, scene);
+
+    sun.material = sunMaterial;
+}
+
+function createPlanet(scene) {
+    const planetMaterial = new BABYLON.StandardMaterial('planetMaterial', scene);
+    planetMaterial.diffuseTexture = new BABYLON.Texture('assets/images/sand.png', scene);
+    planetMaterial.specularColor = BABYLON.Color3.Black();
+
+    const planet = BABYLON.MeshBuilder.CreateSphere('planet', {
+        segments: 16,
+        diameter: 1
+    }, scene);
+    planet.position.x = 6;
+    planet.material = planetMaterial;
+}
+
 function createScene() {
     const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0,-10), scene,);
-    camera.attachControl(canvas, true);
+    scene.clearColor = BABYLON.Color3.Black;
 
-    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
-
-    const box = BABYLON.MeshBuilder.CreateBox('box', {
-        size: 1
-    }, scene);
-
-    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {
-        segments: 32,
-        diameter: 2
-    }, scene);
-    sphere.position = new BABYLON.Vector3(3,0,0);
-
-    const plane = BABYLON.MeshBuilder.CreatePlane('plane', {}, scene);
-    plane.position = new BABYLON.Vector3(-2,0,0);
-
+    createCamera(scene);
+    createLight(scene);
+    createSun(scene);
+    createPlanet(scene);
     return scene;
 }
 
-const scene = createScene();
+const mainScene = createScene();
 
 engine.runRenderLoop(() => {
-    scene.render();
+    mainScene.render();
 })
 
